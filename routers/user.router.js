481ee33d96a852db968f6a8ce829fade5e1c2b84
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2022-10-06 09:18:35 +0300

    upload img, file multer basic

--- routers/user.router.js
+++ routers/user.router.js
@@ -6,6 +6,12 @@ const { User } = require("../models/index");
 
 userRouter.post("/register", register);
 userRouter.post("/login", login);
+//upload image : using multer library to upload the image
+const multer = require("multer");
+const upload = multer({ dest: "./uploads/avatars" });
+userRouter.post("/upload-avatar", upload.single("avatar"), (req, res, next) => {
+  res.send("uploading image function is running successfully");
+});
 
 module.exports = {
   userRouter,
